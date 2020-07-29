20dbb69a1150349a3ca38f9279372684304be56d
deprov447 <ac.10edu@gmail.com>
2020-07-29 12:02:18 +0530

    adding files

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose"),
+    mnLocal = require("passport-local-mongoose");
+
+var userSchema= new mongoose.Schema({
+    username: String,
+    passward: String
+})
+
+userSchema.plugin(mnLocal);
+
+var user = new mongoose.model("user",userSchema);
+
+module.exports= user;
